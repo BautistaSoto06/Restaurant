7d55e4d13d8c5ceb70e19a30bcd0812dcbd4df67
Bautista <bautistasoto918@gmail.com>
2026-08-19 03:15:42 -0300

    Add package.json and Food model class
    - Created package.json to manage project dependencies, adding Express version 5.2.1.
    - Implemented Food model class with properties and a method to retrieve base data.

---
+++ src/models/Food.js
@@ -0,0 +1,23 @@
+export class Food {
+    constructor(id, price, stock, name, image_url, ingeredients, type) {
+        this.id = id;
+        this.price = price;
+        this.stock = stock;
+        this.name = name;
+        this.image_url = image_url;
+        this.ingeredients = ingeredients;
+        this.type = type;
+    }
+
+    getBaseData() {
+        return {
+            id: this.id,
+            name: this.name,
+            price: this.price,
+            stock: this.stock,
+            image_url: this.image_url,
+            ingeredients: this.ingeredients,
+            type: this.type
+        };
+    }
+}
